dba1715ba682586e4f682ec4ea7f19dccec285a2
jbossjaslow <josh.jaslow@gmail.com>
2025-01-21 22:21:04 -0500

    Created WindowedBorderless mod

---
+++ WindowedBorderless/ModConfig.cs
@@ -0,0 +1,39 @@
+﻿using GenericModConfigMenu;
+using StardewModdingAPI;
+
+namespace WindowedBorderless {
+	public class ModConfig {
+		public bool enableForceWindowChangeWithKeybind = false;
+		public SButton forceWindowedBorderlessKeybind = SButton.K;
+
+		public void SetupGenericConfigMenu(IManifest ModManifest, IGenericModConfigMenuApi configMenu) {
+			configMenu.AddBoolOption(
+				mod: ModManifest,
+				name: () => "Enable keybind force",
+				tooltip: () => "Allows forcing the game into windowed borderless mode with a button press",
+				getValue: () => enableForceWindowChangeWithKeybind,
+				setValue: value => enableForceWindowChangeWithKeybind = value,
+				fieldId: ModConfigField.enableForceWindowChangeWithKeybind
+			);
+
+			configMenu.AddKeybind(
+				mod: ModManifest,
+				name: () => "Windowed borderless mode keybind",
+				tooltip: () => "Forces the game into windowed borderless mode",
+				getValue: () => forceWindowedBorderlessKeybind,
+				setValue: value => forceWindowedBorderlessKeybind = value,
+				fieldId: ModConfigField.forceWindowedBorderlessKeybind
+			);
+
+			configMenu.AddParagraph(
+				mod: ModManifest,
+				text: () => "To prevent unneeded behavior, the keybind for forcing the game into windowed borderless mode is locked behind the above flag. To use the keybind, set the flag to True, then use the keybind, and then set it to False again"
+			);
+		}
+	}
+
+	public class ModConfigField {
+		public const string enableForceWindowChangeWithKeybind = "enableForceWindowChangeWithKeybind";
+		public const string forceWindowedBorderlessKeybind = "forceWindowedBorderlessKeybind";
+	}
+}
---
+++ WindowedBorderless/ModEntry.cs
@@ -0,0 +1,64 @@
+﻿using GenericModConfigMenu;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace WindowedBorderless {
+	/// <summary>The mod entry point.</summary>
+	internal sealed class ModEntry : Mod {
+		/*********
+        ** Properties
+        *********/
+		/// <summary>The mod configuration from the player.</summary>
+		private ModConfig Config = new(); // empty constructor here so I don't need to make this nullable
+
+		/*********
+        ** Public methods
+        *********/
+		/// <summary>The mod entry point, called after the mod is first loaded.</summary>
+		/// <param name="helper">Provides simplified APIs for writing mods.</param>
+		public override void Entry(IModHelper helper) {
+			Config = this.Helper.ReadConfig<ModConfig>();
+
+			helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+			helper.Events.Input.ButtonPressed += OnButtonPressed;
+
+			ChangeWindowToWindowedBorderless();
+		}
+
+		/*********
+        ** Private methods
+        *********/
+		private void OnGameLaunched(object? sender, GameLaunchedEventArgs e) {
+			var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+			if (configMenu is null)
+				return;
+
+			// register mod
+			configMenu.Register(
+				mod: this.ModManifest,
+				reset: () => this.Config = new ModConfig(),
+				save: () => this.Helper.WriteConfig(this.Config)
+			);
+			Config.SetupGenericConfigMenu(ModManifest, configMenu);
+		}
+
+		/// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+		/// <param name="sender">The event sender.</param>
+		/// <param name="e">The event data.</param>
+		private void OnButtonPressed(object? sender, ButtonPressedEventArgs e) {
+			if (e.Button == Config.forceWindowedBorderlessKeybind && Config.enableForceWindowChangeWithKeybind) {
+				ChangeWindowToWindowedBorderless();
+			}
+		}
+
+		private void ChangeWindowToWindowedBorderless() {
+			if (!Game1.options.isCurrentlyWindowedBorderless()) {
+				Monitor.Log($"Display is now windowed borderless", LogLevel.Debug);
+				Game1.options.setWindowedOption("Windowed Borderless");
+			} else {
+				Monitor.Log($"Display is already windowed borderless, no need to set it again", LogLevel.Debug);
+			}
+		}
+	}
+}
